795adf6cbba8625c83ddea02d0d70806a8011b2c
Andrii Manieshyn <andreyam92@gmail.com>
2017-09-13 17:34:51 +0300

    Find PI to the Nth Digit
    Bailey-Borwein-Plouffe formula for Pi calculation

--- python_project_list/numbers/src/pi_for_n_digit.py
+++
@@ -1,8 +0,0 @@
-'''
-Find PI to the Nth Digit - Enter a number and have the program generate PI up to that
-many decimal places. Keep a limit to how far the program will go.
-'''
-import math
-
-def find_pi_to_n_digit(n):
-    return format(math.pi, '.' + str(n) + 'f')
---
+++ python_project_list/numbers_tasks/src/pi_for_n_digit.py
@@ -0,0 +1,23 @@
+'''
+Find PI to the Nth Digit - Enter a number and have the program generate PI up to that
+many decimal places. Keep a limit to how far the program will go.
+'''
+
+from decimal import *
+
+def find_pi_to_n_digit(i):
+    getcontext().prec = i + 10
+    calculated_pi = Decimal(0)
+    n = 0
+    while n < i:
+        calculated_pi += (Decimal(1) / (16 ** n)) * ((Decimal(4) / (8 * n + 1)) -
+                                                     (Decimal(2) / (8 * n + 4)) -
+                                                     (Decimal(1) / (8 * n + 5)) -
+                                                     (Decimal(1) / (8 * n + 6)))
+        n += 1
+    return format(str(calculated_pi)[:len(str(calculated_pi)) - 9])
+
+'''
+No explanation for magic numbers "10" and "9" :(
+Something about rounding in formula calculation
+'''
