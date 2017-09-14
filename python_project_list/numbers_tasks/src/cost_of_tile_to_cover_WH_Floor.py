a03c1368b409ef59395b61c1662bae46c8382612
Andrii Manieshyn <andreyam92@gmail.com>
2017-09-14 18:28:51 +0300

    Find Cost of Tile to Cover W x H Floor
    - Parametrized tests
    - Test with raised exception
    - Custom exception

--- python_project_list/numbers_tasks/src/cost_of_tile_to_cover_WH_Floor.py
+++ python_project_list/numbers_tasks/src/cost_of_tile_to_cover_WH_Floor.py
@@ -9,6 +9,10 @@ Strange task and strange solutions in the internet. Too simple
 
 # TODO: raise exception if tiles can not be suited
 
+class TileValidateException(BaseException):
+    def __init__(self, message):
+        self.message = message
+
 def simple_calc_function ():
     return lambda height, width, cost : height * width * cost
 
@@ -16,20 +20,15 @@ def calc_function (**parameters):
     floor_height, floor_width, tile_height, tile_width, tile_cost = \
         parameters['floor_height'], parameters['floor_width'], parameters['tile_height'],\
         parameters['tile_width'], parameters['tile_cost']
-    valid_square = valid(floor_height, floor_width, tile_height, tile_width)
-    if valid_square :
-        amount_of_tiles = calculate_amount(floor_height, floor_width, tile_height, tile_width)
-        return amount_of_tiles * tile_cost
-    else :
-        return None
+    validate(floor_height, floor_width, tile_height, tile_width)
+    return calculate_cost(floor_height, floor_width, tile_height, tile_width, tile_cost)
 
-def valid (*parameters):
+
+def validate (*parameters):
     if (parameters[0] < parameters[2]) or (parameters[1] < parameters[3]):
-        return False
+        raise TileValidateException("Tile is bigger than floor!")
     elif (parameters[0] % parameters[2] != 0) or (parameters[1] % parameters[3] != 0):
-        return False
-    else :
-        return True
+        raise TileValidateException("Can not place tile!")
 
-def calculate_amount(*parameters):
-    return (parameters[0] / parameters[2]) * (parameters[1] / parameters[3])
+def calculate_cost(*parameters):
+    return (parameters[0] / parameters[2]) * (parameters[1] / parameters[3]) * parameters[4]
--- python_project_list/numbers_tasks/test/test_cost_of_tile_to_cover_WH_Floor.py
+++ python_project_list/numbers_tasks/test/test_cost_of_tile_to_cover_WH_Floor.py
@@ -1,1 +1,40 @@
-# TODO: create tests and add test for raised exception
+from nose.tools import assert_equal
+from parameterized import parameterized
+from numbers_tasks.src.cost_of_tile_to_cover_WH_Floor import *
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    @parameterized.expand([
+        (100, 150, 0.5, 7500),
+        (200, 50, 0.1, 1000),
+        (100, 15, 5, 7500)
+    ])
+    def test_simple_solution(self, height, width, cost, result):
+        calc_function = simple_calc_function()
+        assert_equal(calc_function(height, width, cost), result)
+
+
+    @parameterized.expand([
+        ({'floor_height':10, 'floor_width':15, 'tile_height':100, 'tile_width':150, 'tile_cost':15555},)
+    ])
+    def test_calc_function_raise_validation_exception_when_tile_is_bigger_then_floor(self, kwargs):
+        self.assertRaises(TileValidateException, lambda: calc_function(**kwargs))
+
+
+    @parameterized.expand([
+        ({'floor_height': 150, 'floor_width': 100, 'tile_height': 100, 'tile_width': 90, 'tile_cost': 15555},)
+    ])
+    def test_calc_function_raise_validation_exception_when_can_not_place_tile_on_floor(self, kwargs):
+        self.assertRaises(TileValidateException, lambda: calc_function(**kwargs))
+
+
+    @parameterized.expand([
+        (2000,{'floor_height': 100, 'floor_width': 150, 'tile_height': 10, 'tile_width': 15, 'tile_cost': 20}),
+        (100,{'floor_height': 200, 'floor_width': 150, 'tile_height': 100, 'tile_width': 150, 'tile_cost': 50}),
+        (625,{'floor_height': 10, 'floor_width': 15, 'tile_height': 2, 'tile_width': 3, 'tile_cost': 25}),
+    ])
+    def test_calc_function(self, result, kwarg):
+        print(calc_function(**kwarg))
+        self.assertEquals(calc_function(**kwarg), result)
