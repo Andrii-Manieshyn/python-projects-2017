b84192de34a0b98c50ab24e3916840eb32d2413a
Andrii Manieshyn <andreyam92@gmail.com>
2017-09-14 21:59:13 +0300

    Binary to Decimal and Back Converter
    - Using of default python function to convert
    - Tests refactor

--- python_project_list/numbers_tasks/src/binary_to_decimal.py
+++ python_project_list/numbers_tasks/src/binary_to_decimal.py
@@ -1,10 +1,23 @@
 """
 Binary to Decimal and Back Converter
+
 Develop a converter to convert a decimal number to binary or a binary number to its decimal equivalent.
 """
 
 def decimal_to_binary(decimal):
-    pass
+    """
+    Function convert decimal number to binary
+
+    :param decimal: integer number to convert
+    :return: string representation of binary result
+    """
+    return bin(decimal)
 
 def binary_to_decimal(binary):
-    pass
+    """
+    Function convert binary number to integer
+
+    :param binary: string that present binary number
+    :return: integer converted from binary number
+    """
+    return int(binary, 2)
--- python_project_list/numbers_tasks/test/test_binary_to_decimal.py
+++ python_project_list/numbers_tasks/test/test_binary_to_decimal.py
@@ -7,19 +7,19 @@ import unittest
 class ChangeReturnTestCase(unittest.TestCase):
 
     @parameterized.expand([
-        (1, 1),
-        (1001, 9),
-        (110111, 55),
-        (1000000000, 512)
+        ('0b1', 1),
+        ('0b1001', 9),
+        ('0b110111', 55),
+        ('0b1000000000', 512)
     ])
     def test_binary_to_decimal(self, binary, decimal_result):
         assert_equal(binary_to_decimal(binary), decimal_result)
 
     @parameterized.expand([
-        (1, 1),
-        (9, 1001),
-        (55, 110111),
-        (512, 1000000000)
+        (1, '0b1'),
+        (9, '0b1001'),
+        (55, '0b110111'),
+        (512, '0b1000000000')
     ])
     def test_decimal_to_binary(self, decimal, binary_result):
         assert_equal(decimal_to_binary(decimal), binary_result)
